c5f49b36f8f5f58e5f2377f990d0e83cfb752610
Tim McNamara <code@timmcnamara.co.nz>
2021-10-12 20:25:58 +1300

    use requirement.txt

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 python3 -m venv venv
 source venv/bin/activate
 pip install -U pip
-pip install vpype[all]
+pip install -r requirements.txt
 pip install git+https://github.com/abey79/vsketch#egg=vsketch
